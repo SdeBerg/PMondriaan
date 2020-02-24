9ef8eabf96683f07bee5c80795be2ca203f09df7
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2020-02-24 14:05:27 +0100

    Simplify CLI parsing
    - I like having a 'settings struct' that you can pass to functions or capture in
    lambdas.
    - As discussed offline, I have removed the communication of CLI parameters,
      and assume that each processor receives the same argc/argv arguments.
    - Processor '0' still is the only one that writes the passed arguments
    to a defaults file. Please double check that this is the behaviour you
    had before.

--- tools/PMondriaan.cpp
+++ tools/PMondriaan.cpp
@@ -20,25 +20,36 @@ int main(int argc, char **argv) {
 	app.option_defaults()->required();
 	
 	app.set_config("--config", "../tools/defaults.toml", "Read a TOML file", true);
-	
-	int p, k;
-	double eps, eta;
-	std::string matrix_file, hypergraph_weights, bisection_mode, sampling_mode, metric;
+
+  struct cli_settings {
+    int p = 2;
+    int k = 2;
+    double eps = 0.05;
+    double eta = 0.10;
+    std::string matrix_file;
+    std::string hypergraph_weights;
+    std::string bisection_mode;
+    std::string sampling_mode;
+    std::string metric;
+  };
+
+  cli_settings settings;
+
 	auto options = pmondriaan::options();
 	
-	CLI::Option *fopt = app.add_option("-f, --file", matrix_file, "File including the hypergraph to be partitioned in matrixmarket format");
+	CLI::Option *fopt = app.add_option("-f, --file", settings.matrix_file, "File including the hypergraph to be partitioned in matrixmarket format");
 	fopt->check(CLI::ExistingFile);
 	
-	app.add_option("-p, --p", p, "Number of processors to be used");
-	app.add_option("-k, --k", k, "Number of parts to partition H into");
+	app.add_option("-p, --processors", settings.p, "Number of processors to be used", settings.p);
+	app.add_option("-k, --parts", settings.k, "Number of parts to partition H into", settings.k);
 	
-	app.add_option("--eps, --epsilon", eps, "Maximum imbalance of the final partitioning");
-	app.add_option("--eta", eta, "Maximum imbalance during the parallel computation");
+	app.add_option("--eps, --epsilon", settings.eps, "Maximum imbalance of the final partitioning", settings.eps);
+	app.add_option("--eta", settings.eta, "Maximum imbalance during the parallel computation", settings.eta);
 	
-	CLI::Option *wopt = app.add_option("--weights", hypergraph_weights, "How the weights of the vertices should be computed");
-	CLI::Option *bopt = app.add_option("--bisect", bisection_mode, "The bisection mode used");
-	CLI::Option *copt = app.add_option("--sampling", sampling_mode, "Sampling mode to be used");
-	CLI::Option *mopt = app.add_option("--metric", metric, "Metric to optimized");
+	CLI::Option *wopt = app.add_option("--weights", settings.hypergraph_weights, "How the weights of the vertices should be computed");
+	CLI::Option *bopt = app.add_option("--bisect", settings.bisection_mode, "The bisection mode used");
+	CLI::Option *copt = app.add_option("--sampling", settings.sampling_mode, "Sampling mode to be used");
+	CLI::Option *mopt = app.add_option("--metric", settings.metric, "Metric to optimized");
 	
 	wopt->check(CLI::IsMember({"one", "degree"}));
 	bopt->check(CLI::IsMember({"random", "multilevel"}));
@@ -51,84 +62,27 @@ int main(int argc, char **argv) {
 	app.add_option("--coarsening_nrvertices", options.coarsening_nrvertices, "The number of vertices in the hypergraph when coarsening stops");
 	app.add_option("--coarsening_max_rounds", options.coarsening_maxrounds, "The maximum number of coarsening rounds");
 
-
 	CLI11_PARSE(app, argc, argv);
-	
-	/* Write the settings to the defaults file */
-	auto conf = app.config_to_str();
-	std::ofstream out("../tools/settings_run.toml");
-	out << conf;
-	out.close();
 	
 	bulk::thread::environment env;
 	/* Start parallel part */
-	env.spawn(p, [](bulk::world& world) {
-		auto p = world.active_processors();
-		auto s = world.rank();
-		
-		auto q_settings = bulk::queue<int, double, double, std::string, std::string, std::string, std::string, std::string, pmondriaan::options>(world);
+	env.spawn(settings.p, [&settings, &options, &app](bulk::world& world) {
+    auto s = world.rank();
 
-		int k;
-		double eps, eta;
-		std::string matrix_file, hypergraph_weights, bisection_mode, sampling_mode, metric;
+    if (s == 0) {
+      /* Write the settings to the defaults file */
+      auto conf = app.config_to_str();
+      std::ofstream out("../tools/settings_run.toml");
+      out << conf;
+      out.close();
+  }
 
-		auto options = pmondriaan::options();
-		
-		if (s == 0) {
-			
-			/* Sequential reading of parameters */
-			CLI::App app("PMondriaan settings");
-			app.prefix_command(true);
-			app.option_defaults()->required();
-			
-			app.set_config("--config", "../tools/settings_run.toml", "Read a TOML file", true);
-			
-			app.add_option("-f, --file", matrix_file, "File including the hypergraph to be partitioned in matrixmarket format");
-			
-			app.add_option("-k, --k", k, "Number of parts to partition H into");
-			app.add_option("--eps, --epsilon", eps, "Maximum imbalance of the final partitioning");
-			app.add_option("--eta", eta, "Maximum imbalance during the parallel computation");
-			app.add_option("--weights", hypergraph_weights, "How the weights of the vertices should be computed");
-			app.add_option("--bisect", bisection_mode, "The bisection mode used");
-			app.add_option("--sampling", sampling_mode, "Sampling mode to be used");
-			app.add_option("--metric", metric, "Metric to optimized");
-			app.add_option("--sample_size", options.sample_size, "The sample size used in the coarsening");
-			app.add_option("--max_cluster_size", options.coarsening_max_clustersize, "The maximum clustersize during coarsening");
-			app.add_option("--lp_max_iter", options.lp_max_iterations, "The maximum number of iterations in the label propagation");
-			app.add_option("--coarsening_nrvertices", options.coarsening_nrvertices, "The number of vertices in the hypergraph when coarsening stops");
-			app.add_option("--coarsening_max_rounds", options.coarsening_maxrounds, "The maximum number of coarsening rounds");
+		auto H = pmondriaan::read_hypergraph(settings.matrix_file, world, settings.hypergraph_weights);
 
-			const char  arg0[] = "empty";
-			const char* argv[] = { &arg0[0], NULL };
-			int argc = (int)(sizeof(argv) / sizeof(argv[0])) - 1;
-			app.parse(argc, &argv[0]);
-			
-			for (int t = 0; t < p; t++) {
-				q_settings(t).send(k, eps, eta, matrix_file, hypergraph_weights, bisection_mode, sampling_mode, metric, options);
-			}
-			
-		}
+		recursive_bisect(world, H, settings.bisection_mode, settings.sampling_mode, settings.metric, settings.k, settings.eps, settings.eta, options);
 		
-		world.sync();
-		
-		for (const auto& [k_, eps_, eta_, matrix_file_, hypergraph_weights_, bisection_mode_, sampling_mode_, metric_, options_] : q_settings) {
-            k = k_;
-			eps = eps_;
-			eta = eta_;
-			matrix_file = matrix_file_;
-			hypergraph_weights = hypergraph_weights_;
-			bisection_mode = bisection_mode_;
-			sampling_mode = sampling_mode_;
-			metric = metric_;
-			options = options_;
-        }
-		
-		auto H = pmondriaan::read_hypergraph(matrix_file, world, hypergraph_weights);
-
-		recursive_bisect(world, H, bisection_mode, sampling_mode, metric, k, eps, eta, options);
-		
-		auto lb = pmondriaan::load_balance(world, H, k);
-		auto cutsize = pmondriaan::cutsize(world, H, k, metric);
+		auto lb = pmondriaan::load_balance(world, H, settings.k);
+		auto cutsize = pmondriaan::cutsize(world, H, settings.k, settings.metric);
 		
 		if (s == 0) {
 			world.log("Load balance of partitioning found: %lf", lb);
