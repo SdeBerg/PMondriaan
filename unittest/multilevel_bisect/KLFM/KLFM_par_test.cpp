778097f49c7f1ccb75560efd2c95f6b47055a121
SdeBerg <sarita.deberg@gmail.com>
2020-04-17 15:03:29 +0200

    Add unittest InitPreviousC

--- unittest/multilevel_bisect/KLFM/KLFM_par_test.cpp
+++ unittest/multilevel_bisect/KLFM/KLFM_par_test.cpp
@@ -45,5 +45,40 @@ TEST(KLFMParallel, InitCounts) {
     });
 }
 
+TEST(KLFMParallel, InitPreviousC) {
+    environment env;
+    env.spawn(2, [](bulk::world& world) {
+        auto s = world.rank();
+        std::stringstream mtx_ss(mtx_three_nonzeros);
+        auto hypergraph = read_hypergraph_istream(mtx_ss, world, "degree");
+        auto H = hypergraph.value();
+        if (s == 0) {
+            H(0).set_part(0);
+            H(1).set_part(1);
+        }
+        if (s == 1) {
+            H(0).set_part(1);
+        }
+        auto C = pmondriaan::init_counts(world, H);
+        ASSERT_EQ(C[0][0], 1);
+        ASSERT_EQ(C[0][1], 1);
+        if (s == 0) {
+            ASSERT_EQ(C[1][0], 1);
+            ASSERT_EQ(C[1][1], 1);
+        }
+        auto net_partition = bulk::block_partitioning<1>({H.global_number_nets()}, 2);
+        auto previous_C = bulk::coarray<long>(world, net_partition.local_count(s) * 2);
+        auto cost_my_nets = bulk::coarray<long>(world, net_partition.local_count(s));
+        auto cut_size_my_nets =
+        init_previous_C(world, H, C, previous_C, cost_my_nets, net_partition);
+        if (s == 0) {
+            ASSERT_EQ(cut_size_my_nets, 2);
+        }
+        if (s == 1) {
+            ASSERT_EQ(cut_size_my_nets, 0);
+        }
+    });
+}
+
 } // namespace
 } // namespace pmondriaan
