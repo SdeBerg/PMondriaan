6d30ee6b259b928c30ce00ecef2f308a981123c4
SdeBerg <sarita.deberg@gmail.com>
2020-04-30 14:42:53 +0200

    Change interval operator () function

--- include/util/interval.hpp
+++ include/util/interval.hpp
@@ -9,7 +9,13 @@ struct interval {
     long high;
     std::array<long, 2> values = {low, high};
     long length() { return high - low; }
-    long operator()(long part) { return values[part]; }
+    long operator()(long part) {
+        if (part == 0) {
+            return low;
+        } else {
+            return high;
+        }
+    }
 };
 
 } // namespace pmondriaan
